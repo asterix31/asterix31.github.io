ec7a2ddb2364838149feec274abe486245819bc4
asterix31 <sebastien.couderc@live.fr>
2022-12-14 18:51:00 -1000

    update with new pages

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def homepage():
     return render_template("homepage.html")
+
+@app.route("/products", methods=["POST"])
+def products():
+    return render_template("products.html")
+
+@app.route("/tourisme", methods=["POST"])
+def tourisme():
+    return render_template("tourisme.html")
+
+@app.route("/contacts", methods=["POST"])
+def contacts():
+    return render_template("contacts.html")
